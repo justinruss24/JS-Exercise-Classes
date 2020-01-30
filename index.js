2b446efe4bf1f568770bb52861bc0f946346f3dc
Justin Russell <Justin@Justins-MacBook.local>
2020-01-30 12:42:50 -0800

    moving along

--- index.js
+++ index.js
@@ -111,6 +111,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 
 }
 
@@ -128,7 +136,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(instructAttr) {
+    super(instructAttr)
+    this.specialty = instructAttr.specialty;
+    this.favLanguage = instructAttr.favLanguage;
+    this.catchPhrase = instructAttr.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`;
+  };
+  grade(student, sub) {
+    return `${student} receives a perfect score on ${sub}`;
+  }
+
 
 }
 
@@ -147,8 +168,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(studentAttr) {
+    super(studentAttr);
+    this.previousBackground = studentAttr.previousBackground;
+    this.className = studentAttr.className;
+    this.favSubjects = studentAttr.favSubjects;
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}.join(',')`;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(sub) {
+    return `${this.name} has begun a sprint challenge on ${sub}`;
+  }
 }
 
 /*
